440529a9e0d537d71bdd1b60b16edeec33894ca6
Herman Gohn <herman.gohn@elas.ai>
2023-12-01 17:53:37 +0100

    Slight refactor before part 2

---
+++ 1_trebuchet/bin/main.ml
@@ -0,0 +1,20 @@
+open Base
+open Stdio
+let find_bookend_numbers (s: string) : string option =
+  let num_string = String.filter s ~f:Char.is_digit in
+  let string_length = String.length num_string in
+  match string_length with
+    | 0 -> None
+    | 1 -> let digit_char = String.get num_string 0 in Some(String.of_char_list [digit_char; digit_char])
+    | _ -> let (first_num_char, last_num_char) = (String.get num_string 0, String.get num_string (string_length - 1)) in
+      Some(String.of_char_list [first_num_char; last_num_char])
+
+let () = 
+
+  let input_lines = Stdio.In_channel.read_lines "input.txt" in
+  let input_number_strings = List.map (List.filter_map input_lines ~f:find_bookend_numbers) ~f:String.strip in
+  Stdio.Out_channel.write_lines "output.txt" input_number_strings;
+
+  let input_numbers = List.map input_number_strings ~f:Int64.of_string in
+  let input_sum = List.fold input_numbers ~init:Int64.zero ~f:Int64.(+) in
+  Stdio.Out_channel.write_all "result.txt" ~data:(Int64.to_string input_sum);
---
+++ 1_trebuchet/lib/trebuchet_lib.ml
@@ -0,0 +1,38 @@
+open Base
+open Base.Bool
+open Stdio
+
+let log_folder = "logs/"
+type log_variant = [`Input | `Output | `Result ]
+
+let get_log_name = function
+  | `Input -> "input.txt"
+  | `Output -> "output.txt"
+  | `Result -> "result.txt"
+
+let get_log_path (target: log_variant) : string = String.concat [log_folder; get_log_name target]
+
+let find_bookend_numbers (s: string) : string option =
+  let num_string = String.filter s ~f:Char.is_digit in
+  let string_length = String.length num_string in
+  match string_length with
+    | 0 -> None
+    | 1 -> let digit_char = String.get num_string 0 in Some(String.of_char_list [digit_char; digit_char])
+    | _ -> let (first_num_char, last_num_char) = (String.get num_string 0, String.get num_string (string_length - 1)) in
+      Some(String.of_char_list [first_num_char; last_num_char])
+
+let decode_input : string list = let log_path = get_log_path `Input in
+  let raw_lines = Stdio.In_channel.read_lines ?fix_win_eol:(Some true) log_path in
+  let raw_digit_lines = List.filter_map raw_lines ~f:find_bookend_numbers in
+  List.map raw_digit_lines ~f:String.strip
+
+let parse_input : int64 list = List.map decode_input ~f:Int64.of_string
+
+let get_result : string = let sum = List.fold parse_input ~init:Int64.zero ~f:Int64.(+) in Int64.to_string sum
+
+(* Probably a wiser way to do this *)
+let generate_log : (_) = function
+    | `Input -> raise (Invalid_argument "input log is read only")
+    | `Output -> let content = decode_input in Stdio.Out_channel.write_lines (get_log_path `Output) content
+    | `Result -> let content = get_result in Stdio.Out_channel.write_all (get_log_path `Result) ~data:content
+  
