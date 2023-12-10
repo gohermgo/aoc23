00780e0026542d9d6cc3851fe205adaeddf450db
gohermgo <gohnherman@gmail.com>
2023-12-10 01:42:15 +0100

    started day 5

--- 5_if_you_give_a_seed_a_fertilizer/bin/main.ml
+++ 5_if_you_give_a_seed_a_fertilizer/bin/main.ml
@@ -1,2 +1,8 @@
 open Core
-let () = print_endline "Hello, World!"
+
+let input_file_data = 
+    String.split_lines (In_channel.read_all "input.txt")
+
+let () = List.iteri input_file_data ~f:(
+    fun i s -> Stdio.print_endline (String.concat_array [|Int.to_string i; " "; s; "\n---"|])
+  )
